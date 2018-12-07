55494c2330612075f8048b9a4c8233b7b0688ecc
adsarode <adsarode@gmail.com>
2018-12-07 15:16:45 +0530

    AStar Algorithm for path finding

---
+++ Graph/AStar.py
@@ -0,0 +1,28 @@
+"""
+    Requires PriorityQueue implementation.
+    Problem is object of custom data structure, but it is easy to replace it with different data structure
+
+    If you diff function definition with that of UCS, Priority is changed from nextCost to nextCost + Heuristic cost
+"""
+
+def AStarSearch(problem):
+    frontierStack = util.PriorityQueue()
+    path, visited = list(), set()
+    currentNode = problem.getStartState()
+    pathCost = 0
+    from game import Directions
+    while not problem.isGoalState(currentNode):
+        if currentNode not in visited:
+            visited.add(currentNode)
+            for next, dirn, cost in problem.getSuccessors(currentNode):
+                if next not in visited:
+                    nextPath = path + [dirn]
+                    nextCost = pathCost + cost
+                    frontierStack.push((nextCost, next, nextPath), nextCost)
+
+        if frontierStack.isEmpty():
+           print('Faied to find path')
+           return []   # Path Not found - failed to reach goal
+        pathCost, currentNode, path = frontierStack.pop()
+
+    return  path
